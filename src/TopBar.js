0fa125b3ff54d9e3af01274c252b1bd4f03a3556
iamjithinjohn <iamjithinjohn@gmail.com>
2022-08-11 03:41:11 +0530

    Add dropdown

--- src/TopBar.js
+++ src/TopBar.js
@@ -2,8 +2,10 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import './TopBar.css'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+;
 
 function TopBar() {
+
     return (
         <div className="TopBar">
             <div className="SearchBar">
@@ -16,9 +18,18 @@ function TopBar() {
             </div>
 
             <div className="UserAccountsContainer">
+                <label for="cars">Choose a car:</label>
+
+                <select name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
             </div>
 
             <div className="CircleNameContainer">
+
             </div>
         </div>
     )
