8aeafbf5348b006536594912e785b25854741a76
iamjithinjohn <iamjithinjohn@gmail.com>
2022-08-11 02:37:55 +0530

    Add Notification Icon

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import './TopBar.css'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 
 function TopBar() {
     return (
@@ -8,6 +9,16 @@ function TopBar() {
             <div className="SearchBar">
                 <SearchIcon />
                 <input type="text" placeholder="Search for movies, TV shows..." />
+            </div>
+
+            <div className="NotificationsContainer">
+                <NotificationsNoneIcon />
+            </div>
+
+            <div className="UserAccountsContainer">
+            </div>
+
+            <div className="CircleNameContainer">
             </div>
         </div>
     )
